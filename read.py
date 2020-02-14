6d208e338b6843113cc972b46589e4137e30fc90
Feather <s5648286819@gmail.com>
2020-02-14 21:10:03 +0800

    clac avg length of reviews

--- read.py
+++ read.py
@@ -8,7 +8,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 10000 == 0:
 			print(len(data))
 
-print(len(data))
-print(data[0])
-print('-----------------------------')
-print(data[1])
+print('檔案讀取完畢，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+avg_len = sum_len / len(data)
+print('留言的平均長度為', avg_len)
