d5b40e6f477c2d67f3141288540040398b0901ba
Feather <s5648286819@gmail.com>
2020-02-14 21:12:34 +0800

    teacher's version

--- read.py
+++ read.py
@@ -13,5 +13,4 @@ print('檔案讀取完畢，總共有', len(data), '筆資料')
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-avg_len = sum_len / len(data)
-print('留言的平均長度為', avg_len)
+print('留言的平均長度為', sum_len / len(data))
